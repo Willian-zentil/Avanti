582f82a23de30467d249120cbe4e4ecd746375d6
Willian-zentil <wzentil@gmail.com>
2021-10-10 10:00:34 -0300

    header / banner desk

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import _ from 'lodash';
+import './style.scss' ; 
+/*import Icon from './dark.png';*/
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
